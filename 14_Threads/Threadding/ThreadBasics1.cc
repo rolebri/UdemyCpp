b59549d980393941e1b92260b18c31568a8327c2
franneck94 <schaffranek.jan@web.de>
2022-12-15 09:57:15 +0100

    ip

--- 14_Threads/Threadding/ThreadBasics1.cc
+++ 14_Threads/Threadding/ThreadBasics1.cc
@@ -3,14 +3,14 @@
 
 void function()
 {
-    auto id = std::this_thread::get_id();
+    const auto id = std::this_thread::get_id();
 
     std::cout << "ID from *function*: " << id << '\n';
 }
 
 int main()
 {
-    auto id = std::this_thread::get_id();
+    const auto id = std::this_thread::get_id();
     std::cout << "ID from *main*: " << id << '\n';
 
     std::thread t1(function);
