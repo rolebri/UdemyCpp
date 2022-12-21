0e26c31541a8f72cf3cd7b60d77798e7bb504ef8
franneck94 <schaffranek.jan@web.de>
2022-12-21 14:59:36 +0100

    up

--- 05_Vector/EraseRemove.cc
+++ 05_Vector/EraseRemove.cc
@@ -6,17 +6,16 @@ void print_vector(const std::vector<int> &my_vector)
 {
     for (std::size_t i = 0; i < my_vector.size(); ++i)
     {
-        std::cout << my_vector[i] << std::endl;
+        std::cout << my_vector[i] << '\n';
     }
-    std::cout << std::endl;
+    std::cout << '\n';
 }
 
 void print_vector_info(const std::vector<int> &my_vector)
 {
-    std::cout << "Size: " << my_vector.size() << std::endl;
-    std::cout << "Capacity: " << my_vector.capacity() << std::endl;
-    std::cout << "Empty?: " << std::boolalpha << my_vector.empty() << std::endl
-              << std::endl;
+    std::cout << "Size: " << my_vector.size() << '\n';
+    std::cout << "Capacity: " << my_vector.capacity() << '\n';
+    std::cout << "Empty?: " << std::boolalpha << my_vector.empty() << "\n\n";
 }
 
 bool is_negative(const int val)
@@ -34,7 +33,8 @@ int main()
     my_vector.erase(remove_it1, my_vector.end());
     print_vector(my_vector);
 
-    auto remove_it2 = std::remove_if(my_vector.begin(), my_vector.end(), is_negative);
+    auto remove_it2 =
+        std::remove_if(my_vector.begin(), my_vector.end(), is_negative);
     my_vector.erase(remove_it2, my_vector.end());
     print_vector(my_vector);
 
