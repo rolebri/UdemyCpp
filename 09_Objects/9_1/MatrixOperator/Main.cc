1ceb55bac9024fa2caf128f3c5a18f9914bef83b
franneck94 <schaffranek.jan@web.de>
2022-12-23 10:04:18 +0100

    up

--- 09_Objects/9_1/MatrixOperator/Main.cc
+++ 09_Objects/9_1/MatrixOperator/Main.cc
@@ -4,15 +4,23 @@
 
 int main()
 {
-    Matrix m1(1.0, 2.0, 3.0, 4.0);
-    Matrix m2(-1.0, -2.0, -3.0, -4.0);
+    auto m1 = Matrix<double>{1.0, 2.0, 3.0, 4.0};
+    m1.print_matrix();
 
-    auto m3 = m1 + m2;
-    m3.print_matrix();
-    auto m4 = m1 + m3;
-    m4.print_matrix();
-    m2 -= m1;
+    auto m2 = Matrix<double>{1.0, 2.0, 3.0, 4.0};
     m2.print_matrix();
+
+    const auto m3 = m1 + m2;
+    m3.print_matrix();
+
+    const auto m4 = m1 - m2;
+    m4.print_matrix();
+
+    m1 += m2;
+    m1.print_matrix();
+
+    m1 -= m2;
+    m1.print_matrix();
 
     return 0;
 }
--- 09_Objects/9_1/MatrixTemplate/Matrix.h
+++ 09_Objects/9_1/MatrixTemplate/Matrix.h
@@ -8,11 +8,6 @@ public:
     Matrix(const T &A, const T &B, const T &C, const T &D);
     ~Matrix() = default;
 
-    Matrix operator+(const Matrix &rhs) const;
-    Matrix operator+=(const Matrix &rhs);
-    Matrix operator-(const Matrix &rhs) const;
-    Matrix operator-=(const Matrix &rhs);
-
     void print_matrix() const;
 
     T get_A() const;
@@ -41,54 +36,6 @@ template <typename T>
 Matrix<T>::Matrix(const T &A, const T &B, const T &C, const T &D)
     : m_A(A), m_B(B), m_C(C), m_D(D)
 {
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator+(const Matrix<T> &rhs) const
-{
-    auto result = Matrix{};
-
-    result.set_A(this->get_A() + rhs.get_A());
-    result.set_B(this->get_B() + rhs.get_B());
-    result.set_C(this->get_C() + rhs.get_C());
-    result.set_D(this->get_D() + rhs.get_D());
-
-    return result;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator+=(const Matrix<T> &rhs)
-{
-    this->set_A(this->get_A() + rhs.get_A());
-    this->set_B(this->get_B() + rhs.get_B());
-    this->set_C(this->get_C() + rhs.get_C());
-    this->set_D(this->get_D() + rhs.get_D());
-
-    return *this;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-(const Matrix<T> &rhs) const
-{
-    auto result = Matrix{};
-
-    result.set_A(this->get_A() - rhs.get_A());
-    result.set_B(this->get_B() - rhs.get_B());
-    result.set_C(this->get_C() - rhs.get_C());
-    result.set_D(this->get_D() - rhs.get_D());
-
-    return result;
-}
-
-template <typename T>
-Matrix<T> Matrix<T>::operator-=(const Matrix<T> &rhs)
-{
-    this->set_A(this->get_A() - rhs.get_A());
-    this->set_B(this->get_B() - rhs.get_B());
-    this->set_C(this->get_C() - rhs.get_C());
-    this->set_D(this->get_D() - rhs.get_D());
-
-    return *this;
 }
 
 template <typename T>
