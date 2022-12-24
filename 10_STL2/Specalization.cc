a6475eca23506b114907b4dbd70b40b6831abd32
franneck94 <schaffranek.jan@web.de>
2022-12-24 13:10:53 +0100

    up

--- 10_STL2/Specalization.cc
+++ 10_STL2/Specalization.cc
@@ -1,33 +1,25 @@
+#include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <string_view>
 #include <vector>
 
 template <typename T>
-struct is_numeric
-    : public std::disjunction<std::is_integral<T>, std::is_floating_point<T>>
-{
-};
-
-template <typename T>
 std::size_t length(const std::vector<T> &vec)
 {
-    static_assert(is_numeric<T>::value, "failed...");
-
     return vec.size();
 }
 
 template <>
 std::size_t length(const std::vector<std::string> &vec)
 {
-    auto l = std::size_t{};
-
-    for (const auto &s : vec)
-    {
-        l += s.size();
-    }
-
-    return l;
+    return std::accumulate(vec.begin(),
+                           vec.end(),
+                           0,
+                           [](const auto prev_len, const auto &str) {
+                               return prev_len + str.size();
+                           });
 }
 
 int main()
