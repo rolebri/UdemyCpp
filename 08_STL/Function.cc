6e603e421ce3182decfe431049aa852e77ff00b1
franneck94 <schaffranek.jan@web.de>
2022-12-24 10:38:46 +0100

    up

--- 08_STL/Function.cc
+++ 08_STL/Function.cc
@@ -8,6 +8,11 @@ std::int32_t f(const std::function<std::int32_t(const std::int32_t)> &callback)
     return callback(2);
 }
 
+std::int32_t h(const std::int32_t v)
+{
+    return v * 2;
+}
+
 int main()
 {
     const auto result1 = f([](const std::int32_t v) { return v * 2; });
@@ -16,6 +21,9 @@ int main()
     auto l = [](const std::int32_t v) { return v * 2; };
     const auto result2 = f(l);
     std::cout << result2 << '\n';
+
+    const auto result3 = f(h);
+    std::cout << result3 << '\n';
 
     const std::int32_t threshold = 2;
     auto fns = std::vector<std::function<bool(const std::int32_t)>>{
